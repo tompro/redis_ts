872f8c605aec9eb6ba0d1379fb27e7149290c443
tompro <office@protom.eu>
2020-06-12 12:16:42 +0200

    Impl. all async and async test

--- src/async_commands.rs
+++ src/async_commands.rs
@@ -30,6 +30,112 @@ pub trait AsyncTsCommands: ConnectionLike + Send + Sized {
         Box::pin(async move { cmd("TS.ADD").arg(key).arg("*").arg(value).query_async(self).await })
     }
 
+    /// Adds a single time series value to a redis time series. If the time series does not 
+    /// yet exist it will be created with given settings.
+    fn ts_add_create<'a, K: ToRedisArgs + Send + Sync + 'a, TS: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, 
+    	RV: FromRedisValue>(&'a mut self, key: K, ts: TS, value: V, options: TsOptions) -> RedisFuture<RV> {
+    		Box::pin(async move { cmd("TS.ADD").arg(key).arg(ts).arg(value).arg(options).query_async(self).await })
+    }
+
+    /// Adds multiple time series values to an existing redis time series.
+    fn ts_madd<'a, K: ToRedisArgs + Send + Sync + 'a, TS: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, 
+    	RV: FromRedisValue>(&'a mut self, values:&'a[(K, TS, V)]) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.MADD").arg(values).query_async(self).await })
+    }
+
+    /// Increments a time series value with redis system time.
+    fn ts_incrby_now<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, RV: FromRedisValue>(
+        &'a mut self, key: K, value: V) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.INCRBY").arg(key).arg(value).query_async(self).await })
+    }
+
+    /// Increments a time series value with given timestamp.
+    fn ts_incrby<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, TS:ToRedisArgs + Send + Sync + 'a, 
+    	RV: FromRedisValue>(&'a mut self, key: K, ts:TS, value: V) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.INCRBY").arg(key).arg(value).arg("TIMESTAMP").arg(ts).query_async(self).await })
+    }
+
+    /// Increments a time series value with timestamp. Time series will be created if it 
+    /// not already exists.
+    fn ts_incrby_create<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, TS:ToRedisArgs + Send + Sync + 'a, 
+    	RV: FromRedisValue>(&'a mut self, key: K, ts:TS, value: V, options:TsOptions) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.INCRBY").arg(key).arg(value).arg("TIMESTAMP")
+            .arg(ts).arg(options).query_async(self).await })
+    }
+
+    /// Decrements a time series value with redis system time.
+    fn ts_decrby_now<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, RV: FromRedisValue>(
+        &'a mut self, key: K, value: V) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.DECRBY").arg(key).arg(value).query_async(self).await })
+    }
+
+    /// Decrements a time series value with given timestamp.
+    fn ts_decrby<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, TS:ToRedisArgs + Send + Sync + 'a, 
+    	RV: FromRedisValue>(&'a mut self, key: K, ts:TS, value: V) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.DECRBY").arg(key).arg(value).arg("TIMESTAMP").arg(ts).query_async(self).await })
+    }
+
+    /// Decrements a time series value with timestamp. Time series will be created if it 
+    /// not already exists.
+    fn ts_decrby_create<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a, TS:ToRedisArgs + Send + Sync + 'a, 
+    	RV: FromRedisValue>(&'a mut self, key: K, ts:TS, value: V, options:TsOptions) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.DECRBY").arg(key).arg(value).arg("TIMESTAMP")
+            .arg(ts).arg(options).query_async(self).await })
+    }
+
+    /// Creates a new redis time series compaction rule.
+    fn ts_createrule<'a, K: ToRedisArgs + Send + Sync + 'a, RV: FromRedisValue>(
+        &'a mut self, source_key: K, dest_key: K, aggregation_type:TsAggregationType) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.CREATERULE").arg(source_key).arg(dest_key).arg(aggregation_type).query_async(self).await})
+    }
+
+    /// Deletes an existing redis time series compaction rule.
+    fn ts_deleterule<'a, K: ToRedisArgs + Send + Sync + 'a, RV: FromRedisValue>(
+        &'a mut self, source_key: K, dest_key: K) -> RedisFuture<RV> {
+        Box::pin(async move { cmd("TS.DELETERULE").arg(source_key).arg(dest_key).query_async(self).await })
+    }
+
+    /// Returns the latest (current) value in a redis time series.
+    fn ts_get<'a, K:ToRedisArgs + Send + Sync + 'a, TS: FromRedisValue, V:FromRedisValue>(&'a mut self, key:K) -> RedisFuture<Option<(TS,V)>> {
+        Box::pin(async move { cmd("TS.GET").arg(key).query_async(self).await.or_else(|_| { Ok(None) }) })
+    }
+
+    /// Returns the latest (current) value from multiple redis time series.
+    fn ts_mget<'a, TS: std::default::Default +  FromRedisValue, V: std::default::Default + FromRedisValue>(
+        &'a mut self, filter_options:TsFilterOptions) -> RedisFuture<TsMget<TS,V>> {
+        Box::pin(async move { cmd("TS.MGET").arg(filter_options).query_async(self).await })
+    }
+
+    /// Executes a redis time series range query.
+    fn ts_range<'a, K:ToRedisArgs + Send + Sync + 'a, FTS:ToRedisArgs + Send + Sync + 'a, TTS:ToRedisArgs + Send + Sync + 'a, 
+    	C:ToRedisArgs + Send + Sync + 'a, TS: std::marker::Copy + FromRedisValue, V: std::marker::Copy + FromRedisValue>(
+        &'a mut self, key:K, from_timestamp:FTS, to_timestamp:TTS, count:Option<C>,aggregation_type:Option<TsAggregationType>) -> RedisFuture<TsRange<TS,V>> {
+        let mut c = cmd("TS.RANGE");
+        c.arg(key).arg(from_timestamp).arg(to_timestamp);
+        if let Some(ct) = count {
+            c.arg("COUNT").arg(ct);
+        }
+        Box::pin(async move { c.arg(aggregation_type).query_async(self).await })
+    }
+
+    /// Executes multiple redis time series range queries.
+    fn ts_mrange<'a, FTS:ToRedisArgs + Send + Sync + 'a, TTS:ToRedisArgs + Send + Sync + 'a, C:ToRedisArgs + Send + Sync + 'a, 
+    TS: std::default::Default + FromRedisValue + Copy, V: std::default::Default + FromRedisValue + Copy>(
+        &mut self, from_timestamp:FTS, to_timestamp:TTS, count:Option<C>, aggregation_type:Option<TsAggregationType>, 
+        filter_options:TsFilterOptions) -> RedisFuture<TsMrange<TS,V>> {
+        let mut c = cmd("TS.MRANGE");
+        c.arg(from_timestamp).arg(to_timestamp);
+        if let Some(ct) = count {
+            c.arg("COUNT").arg(ct);
+        }
+        c.arg(aggregation_type).arg(filter_options);
+        Box::pin(async move { c.query_async(self).await })
+    }
+
+    /// Returns a filtered list of redis time series keys.
+    fn ts_queryindex<'a>(&'a mut self, filter_options:TsFilterOptions) -> RedisFuture<Vec<String>> {
+        Box::pin(async move { cmd("TS.QUERYINDEX").arg(filter_options.get_filters()).query_async(self).await })
+    }
 }
 
 impl<T> AsyncTsCommands for T where T: Send + ConnectionLike {}
--- tests/test_async_commands.rs
+++ tests/test_async_commands.rs
@@ -2,12 +2,13 @@ extern crate redis;
 extern crate redis_ts;
 extern crate async_std;
 
+use std::thread;
 use redis::aio::Connection;
 use redis_ts::AsyncTsCommands;
-use redis_ts::{TsInfo, TsOptions};
+use redis_ts::{TsInfo, TsOptions, TsAggregationType, TsMget, TsFilterOptions, TsRange, TsMrange};
 use async_std::task;
 use redis::AsyncCommands;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
 
 async fn get_con() -> Connection {
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
@@ -19,6 +20,11 @@ async fn prepare_ts(name: &str) -> Connection {
 	let _:() = con.del(name).await.unwrap();
 	let _:() = con.ts_create(name, TsOptions::default()).await.unwrap();
 	con
+}
+
+fn sleep(ms:u64) {
+    let millis = Duration::from_millis(ms);
+    thread::sleep(millis);
 }
 
 #[test]
@@ -55,4 +61,285 @@ fn test_ts_add_now() {
 		ts
 	});
 	assert!(now <= ts);
+}
+
+#[test]
+fn test_ts_add_create() {
+	let _:() = task::block_on(async {
+		let mut con = get_con().await;
+		let _:() = con.del("async_test_ts_add_create").await.unwrap();
+		let ts:u64 = con.ts_add_create(
+        "async_test_ts_add_create", 1234567890, 2.2, TsOptions::default()
+    ).await.unwrap();
+		assert_eq!(ts, 1234567890);
+
+		let ts2:u64 = con.ts_add_create(
+        "async_test_ts_add_create", "*", 2.3, TsOptions::default()
+    ).await.unwrap();
+    assert!(ts2 > ts);
+	});
+}
+
+#[test]
+fn test_ts_madd() {
+	let _:() = task::block_on(async {
+		let mut con = prepare_ts("async_test_ts_madd").await;
+		let _ = prepare_ts("async_test_ts_madd2").await;
+		let expected:Vec<u64> = vec![1234, 4321];
+		let res:Vec<u64> = con.ts_madd(
+			&[("async_test_ts_madd", 1234, 1.0), ("async_test_ts_madd2", 4321, 2.0)]
+		).await.unwrap();
+		assert_eq!(expected, res);
+	});
+}
+
+#[test]
+fn test_ts_incrby_now() {
+	let _:() = task::block_on(async {
+		let mut con = prepare_ts("async_test_ts_incrby_now").await;
+		let _:() = con.ts_incrby_now("async_test_ts_incrby_now", 1).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_incrby_now").await.unwrap();
+    assert_eq!(v1.unwrap().1, 1.0);
+    sleep(1);
+    let _:() = con.ts_incrby_now("async_test_ts_incrby_now", 5).await.unwrap();
+    let v2:Option<(u64,f64)> = con.ts_get("async_test_ts_incrby_now").await.unwrap();
+    assert_eq!(v2.unwrap().1, 6.0);
+	});
+}
+
+#[test]
+fn test_ts_decrby_now() {
+	let _:() = task::block_on(async {
+		let mut con = prepare_ts("async_test_ts_decrby_now").await;
+		let _:() = con.ts_add_now("async_test_ts_decrby_now", 10).await.unwrap();
+		let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby_now").await.unwrap();
+    assert_eq!(v1.unwrap().1, 10.0);
+    sleep(1);
+
+    let _:() = con.ts_decrby_now("async_test_ts_decrby_now", 1).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby_now").await.unwrap();
+    assert_eq!(v1.unwrap().1, 9.0);
+    sleep(1);
+
+    let _:() = con.ts_decrby_now("async_test_ts_decrby_now", 5).await.unwrap();
+    let v2:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby_now").await.unwrap();
+    assert_eq!(v2.unwrap().1, 4.0);
+	});    
+}
+
+#[test]
+fn test_ts_incrby() {
+	let _:() = task::block_on(async {
+    let mut con = prepare_ts("async_test_ts_incrby").await;
+    
+    let _:() = con.ts_incrby("async_test_ts_incrby", 123, 1).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_incrby").await.unwrap();
+    assert_eq!(v1.unwrap(), (123, 1.0));
+
+    let _:() = con.ts_incrby("async_test_ts_incrby", 1234, 5).await.unwrap();
+    let v2:Option<(u64,f64)> = con.ts_get("async_test_ts_incrby").await.unwrap();
+    assert_eq!(v2.unwrap(), (1234, 6.0));
+  });
+}
+
+#[test]
+fn test_ts_decrby() {
+	let _:() = task::block_on(async {
+    let mut con = prepare_ts("async_test_ts_decrby").await;
+    let _:() = con.ts_add("async_test_ts_decrby", 12, 10).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby").await.unwrap();
+    assert_eq!(v1.unwrap(), (12, 10.0));
+
+    let _:() = con.ts_decrby("async_test_ts_decrby", 123, 1).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby").await.unwrap();
+    assert_eq!(v1.unwrap(), (123, 9.0));
+
+    let _:() = con.ts_decrby("async_test_ts_decrby", 1234, 5).await.unwrap();
+    let v2:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby").await.unwrap();
+    assert_eq!(v2.unwrap(), (1234, 4.0));
+  });
+}
+
+#[test]
+fn test_ts_incrby_create() {
+	let _:() = task::block_on(async {
+		let mut con = get_con().await;
+    let _:() = con.del("async_test_ts_incrby_create").await.unwrap();
+    
+    let _:() = con.ts_incrby_create("async_test_ts_incrby_create", 123, 1, TsOptions::default()).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_incrby_create").await.unwrap();
+    assert_eq!(v1.unwrap(), (123, 1.0));
+
+    let _:() = con.ts_incrby_create("async_test_ts_incrby_create", 1234, 5, TsOptions::default()).await.unwrap();
+    let v2:Option<(u64,f64)> = con.ts_get("async_test_ts_incrby_create").await.unwrap();
+    assert_eq!(v2.unwrap(), (1234, 6.0));
+  });
+}
+
+#[test]
+fn test_ts_decrby_create() {
+	let _:() = task::block_on(async {
+    let mut con = get_con().await;
+    let _:() = con.del("async_test_ts_decrby_create").await.unwrap();
+
+    let _:() = con.ts_decrby_create("async_test_ts_decrby_create", 123, 1, TsOptions::default()).await.unwrap();
+    let v1:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby_create").await.unwrap();
+    assert_eq!(v1.unwrap(), (123, -1.0));
+
+    let _:() = con.ts_decrby_create("async_test_ts_decrby_create", 1234, 5, TsOptions::default()).await.unwrap();
+    let v2:Option<(u64,f64)> = con.ts_get("async_test_ts_decrby_create").await.unwrap();
+    assert_eq!(v2.unwrap(), (1234, -6.0));
+  });
+}
+
+#[test]
+fn test_ts_create_delete_rule() {
+	let _:() = task::block_on(async {
+    let mut con = prepare_ts("async_test_ts_create_delete_rule").await;
+    let _ = prepare_ts("async_test_ts_create_delete_rule2").await;
+    let _:() = con.ts_createrule(
+        "async_test_ts_create_delete_rule", "async_test_ts_create_delete_rule2", TsAggregationType::Avg(5000)
+    ).await.unwrap();
+
+    let info:TsInfo = con.ts_info("async_test_ts_create_delete_rule").await.unwrap();
+    assert_eq!(info.rules, vec![("async_test_ts_create_delete_rule2".to_string(), 5000, "AVG".to_string())]);
+
+    let _:() = con.ts_deleterule("async_test_ts_create_delete_rule", "async_test_ts_create_delete_rule2").await.unwrap();
+    let info:TsInfo = con.ts_info("async_test_ts_create_delete_rule").await.unwrap();
+    assert_eq!(info.rules, vec![]);
+  });
+}
+
+#[test]
+fn test_ts_get() {
+	let _:() = task::block_on(async {
+    let mut con = prepare_ts("async_test_ts_get").await;
+    let _:() = con.ts_add("async_test_ts_get", 1234, 2.0).await.unwrap();
+    let res:Option<(u64,f64)> = con.ts_get("async_test_ts_get").await.unwrap();
+    assert_eq!(Some((1234, 2.0)), res);
+  });
+}
+
+#[test]
+fn test_ts_mget() {
+	let _:() = task::block_on(async {
+    let mut con = get_con().await;
+    let opts:TsOptions = TsOptions::default().label("l", "async_mget");
+
+    let _:() = con.del("async_test_ts_mget").await.unwrap();
+    let _:() = con.del("async_test_ts_mget2").await.unwrap();
+    let _:() = con.del("async_test_ts_mget3").await.unwrap();
+    
+    let _:() = con.ts_create("async_test_ts_mget", opts.clone()).await.unwrap();
+    let _:() = con.ts_create("async_test_ts_mget2", opts.clone()).await.unwrap();
+    let _:() = con.ts_create("async_test_ts_mget3", opts.clone()).await.unwrap();
+    
+    let _:() = con.ts_madd(&[
+        ("async_test_ts_mget", 12, 1.0), 
+        ("async_test_ts_mget", 123, 2.0),
+        ("async_test_ts_mget", 1234, 3.0),
+        ("async_test_ts_mget2", 21, 1.0), 
+        ("async_test_ts_mget2", 321, 2.0),
+        ("async_test_ts_mget2", 4321, 3.0)
+    ]).await.unwrap();
+    
+    let res:TsMget<u64,f64> = con.ts_mget(
+        TsFilterOptions::default().equals("l", "async_mget").with_labels(true)
+    ).await.unwrap();
+    
+    assert_eq!(res.values.len(), 3);
+    assert_eq!(res.values[0].value, Some((1234, 3.0)));
+    assert_eq!(res.values[1].value, Some((4321, 3.0)));
+    assert_eq!(res.values[2].value, None);
+  });
+}
+
+#[test]
+fn test_ts_get_ts_info() {
+	let _:() = task::block_on(async {
+    let mut con = get_con().await;
+    let _:() = con.del("async_test_ts_get_ts_info").await.unwrap();
+    let _:() = con.ts_create("async_test_ts_get_ts_info", TsOptions::default().label("a", "b")).await.unwrap();
+    let _:() = con.ts_add("async_test_ts_get_ts_info", "1234", 2.0).await.unwrap();
+    let info:TsInfo = con.ts_info("async_test_ts_get_ts_info").await.unwrap();
+    assert_eq!(info.total_samples, 1);
+    assert_eq!(info.first_timestamp, 1234);
+    assert_eq!(info.last_timestamp, 1234);
+    assert_eq!(info.chunk_count, 1);
+    assert_eq!(info.labels, vec![("a".to_string(),"b".to_string())]);
+  });
+}
+
+#[test]
+fn test_ts_range() {
+	let _:() = task::block_on(async {
+    let mut con = prepare_ts("async_test_ts_range").await;
+    let _ = prepare_ts("async_test_ts_range2").await;
+    let _:() = con.ts_madd(&[
+        ("async_test_ts_range", 12, 1.0), 
+        ("async_test_ts_range", 123, 2.0),
+        ("async_test_ts_range", 1234, 3.0)
+    ]).await.unwrap();
+
+    let res:TsRange<u64,f64> = con.ts_range("async_test_ts_range", "-", "+", None::<usize>, None).await.unwrap();
+    assert_eq!(res.values, vec![(12, 1.0), (123, 2.0), (1234, 3.0)]);
+
+    let one_res:TsRange<u64,f64> = con.ts_range("async_test_ts_range", "-", "+", Some(1), None).await.unwrap();
+    assert_eq!(one_res.values, vec![(12, 1.0)]);
+
+    let range_res:TsRange<u64,f64> = con.ts_range("async_test_ts_range", 12, 123, None::<usize>, None).await.unwrap();
+    assert_eq!(range_res.values, vec![(12, 1.0), (123, 2.0)]);
+
+    let sum:TsRange<u64,f64> = con.ts_range("async_test_ts_range", 12, 123, None::<usize>, Some(TsAggregationType::Sum(10000))).await.unwrap();
+    assert_eq!(sum.values, vec![(0, 3.0)]);
+
+    let res:TsRange<u64,f64> = con.ts_range("async_test_ts_range2", "-", "+", None::<usize>, None).await.unwrap();
+    assert_eq!(res.values, vec![]);
+  });
+}
+
+#[test]
+fn test_ts_mrange() {
+	let _:() = task::block_on(async {
+    let mut con = get_con().await;
+    let _:() = con.del("async_test_ts_mrange").await.unwrap();
+    let _:() = con.del("async_test_ts_mrange2").await.unwrap();
+    let opts:TsOptions = TsOptions::default().label("l", "async_mrange");
+    let _:() = con.ts_create("async_test_ts_mrange", opts.clone()).await.unwrap();
+    let _:() = con.ts_create("async_test_ts_mrange2", opts.clone()).await.unwrap();
+    let _:() = con.ts_madd(&[
+        ("async_test_ts_mrange", 12, 1.0), 
+        ("async_test_ts_mrange", 123, 2.0),
+        ("async_test_ts_mrange", 1234, 3.0),
+        ("async_test_ts_mrange2", 21, 1.0), 
+        ("async_test_ts_mrange2", 321, 2.0),
+        ("async_test_ts_mrange2", 4321, 3.0)
+    ]).await.unwrap();
+
+    let res:TsMrange<u64,f64> = con.ts_mrange(
+        "-", "+", None::<usize>, None, TsFilterOptions::default().equals("l", "async_mrange").with_labels(true)
+    ).await.unwrap();
+    assert_eq!(res.values.len(), 2);
+    assert_eq!(res.values[1].values, vec![(21, 1.0), (321, 2.0), (4321, 3.0)]);
+    assert_eq!(res.values[0].key, "async_test_ts_mrange");
+    assert_eq!(res.values[1].key, "async_test_ts_mrange2");
+    assert_eq!(res.values[0].labels, vec![("l".to_string(), "async_mrange".to_string())]);
+
+    let res2:TsMrange<u64,f64> = con.ts_mrange(
+        "-", "+", None::<usize>, None, TsFilterOptions::default().equals("none", "existing").with_labels(true)
+    ).await.unwrap();
+    assert!(res2.values.is_empty());
+  });
+}
+
+#[test]
+fn test_ts_queryindex() {
+	let _:() = task::block_on(async {
+    let mut con = get_con().await;
+    let _:() = con.del("async_test_ts_queryindex").await.unwrap();
+    let _:() = con.ts_create("async_test_ts_queryindex", TsOptions::default().label("a", "b")).await.unwrap();
+    let _:() = con.ts_add("async_test_ts_queryindex", "1234", 2.0).await.unwrap();
+    let index:Vec<String> = con.ts_queryindex(TsFilterOptions::default().equals("a", "b")).await.unwrap();
+    assert!(index.contains(&"async_test_ts_queryindex".to_string()));
+  });
 }
